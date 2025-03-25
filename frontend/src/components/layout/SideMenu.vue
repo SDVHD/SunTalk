0e5d20714fbcf46d4f8d10b5597f9fd3cc1fe3a7
SwissDaVinci <swissdavinci@gmail.com>
2025-03-25 21:31:12 +0100

    Add Navigation

--- frontend/src/components/layout/SideMenu.vue
+++ frontend/src/components/layout/SideMenu.vue
@@ -1,4 +1,6 @@
 <script setup>
+import IconsComp from '../tools/IconsComp.vue';
+import { RouterLink } from 'vue-router';
 const props = defineProps({
     title: String,
 });
@@ -8,6 +10,32 @@ const props = defineProps({
     <div class="p-4">
         <h1 class="text-xl font-bold mb-1">{{ title }}</h1>
         <div class="divider mb-4"></div>
-        <slot></slot>
+        <div class="menu p-4 w-full">
+            <ul>
+                <!-- Menu Header -->
+                <li class="menu-title">
+                    <span>Navigation</span>
+                </li>
+                <!-- Menu Items -->
+                <li>
+                    <RouterLink to="/">
+                        <IconsComp name="Home" />
+                        Dashboard
+                    </RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/users">
+                        <IconsComp name="Users" />
+                        Users
+                    </RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/settings">
+                        <IconsComp name="Settings" />
+                        Settings
+                    </RouterLink>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,11 +2,13 @@
 import MainView from '@/components/MainView.vue';
 import ChannelCard from '@/components/channels/ChannelCard.vue';
 import ChannelLayout from '@/components/channels/ChannelLayout.vue';
+import IconsComp from '@/components/tools/IconsComp.vue';
 </script>
 
 <template>
-    <MainView maincontent-title="Channels">
-        <ChannelLayout gridColumns="5">
+    <MainView maincontent-title="Channels" sidemenu="true" sidemenuTitle="SunTalk">
+        <!-- Main Content -->
+        <ChannelLayout>
             <ChannelCard channelName="Production" memberCount="10" />
             <ChannelCard channelName="Video" memberCount="5" />
             <ChannelCard channelName="Audio" memberCount="3" />
